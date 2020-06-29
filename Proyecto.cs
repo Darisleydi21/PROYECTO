6395ff60ede55fc2d19915babf8c0752e0a233b1
Darisleydi21 <65923702+Darisleydi21@users.noreply.github.com>
2020-06-29 01:55:54 -0400

    Add files via upload

---
+++ Proyecto.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int d;
+            string resultado = "";
+            string maquina = "";
+            string jug1 = "";
+
+            Console.WriteLine("Inserte piedra, papel o tijeras");
+        
+            Random rnd = new Random();
+            d = rnd.Next(1, 3);
+
+            if (d == 1)
+            {
+                maquina = "piedra";
+            }
+            if (d == 2)
+            {
+                maquina = "papel";
+            }
+            if (d == 3)
+            {
+                maquina = "tijeras";
+            }
+
+            jug1 = Console.ReadLine();
+            if (maquina == "piedra")
+            {
+                if (jug1 == "piedra")
+                {
+                    resultado = "Empate";
+                }
+                if (jug1 == "papel")
+                {
+                    resultado = "FELICIDADES, GANASTE";
+                }
+                if (jug1 == "tijeras")
+                    resultado = "PERDISTE, INTENTE DE NUEVO";
+            }
+            if (maquina == "papel")
+            {
+                if (jug1 == "papel")
+                {
+                    resultado = "Empate";
+                }
+                if (jug1 == "tijeras")
+                {
+                    resultado = "FELICIDADES, GANASTE";
+                }
+                if (jug1 == "piedra")
+                    resultado = "PERDISTE, INTENTE DE NUEVO";
+            }
+            if (maquina == "tiejras")
+            {
+                if (jug1 == "tijeras")
+                {
+                    resultado = "Empate";
+                }
+                if (jug1 == "piedra")
+                {
+                    resultado = "FELICIDADES, GANASTE";
+                }
+                if (jug1 == "papel")
+                    resultado = "PERDISTE, INTENTE DE NUEVO";
+            }
+            Console.WriteLine("{0} vs {1}", jug1, maquina);
+            Console.WriteLine(resultado);
+            Console.ReadLine();
+        }
+    }
+}
